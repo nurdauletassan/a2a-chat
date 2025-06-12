acdac0a13a81b61fa0fbc9ce213a54032593d908
nurdauletassan <nurdaasan8@gmail.com>
2025-06-12 18:13:21 +0500

    changed port in front2

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,13 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://167.172.160.167:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://167.172.160.167:5173",
+        "http://167.172.160.167:8000",
+        "http://167.172.160.167"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api' || 'http://167.172.160.167:8000/api',
+    baseURL: 'http://167.172.160.167:8000/api',
     headers: {
         'Content-Type': 'application/json',
     },
